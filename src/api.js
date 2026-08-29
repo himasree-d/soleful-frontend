8653615f6f37c82ea2fc92c2237bb40808ea5daa
Himasree <himasree2507@gmail.com>
2026-08-29 12:14:00 +0530

    Update API URL for production

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Base URL for all API calls
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://soleful-backend-production.up.railway.app/api', 
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
